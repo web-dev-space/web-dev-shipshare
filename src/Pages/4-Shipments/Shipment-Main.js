35553ff2637633a6b51e304b650893a338fd5c38
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-05 14:23:08 -0700

    refractor: rename shipments folder

--- src/Pages/4-Shipments/Shipment-Main.js
+++ src/Pages/4-Shipments/Shipment-Main.js
@@ -1,7 +1,7 @@
 import { useState } from "react";
-import Header from "../../third-party/layouts/dashboard/header"
-import NavVertical from "../../third-party/layouts/dashboard/nav/NavVertical"
-import Main from "../../third-party/layouts/dashboard/Main"
+import Header from "../../third-party/layouts/dashboard/header/index.js"
+import NavVertical from "../../third-party/layouts/dashboard/nav/NavVertical.js"
+import Main from "../../third-party/layouts/dashboard/Main.js"
 import { Container, Typography, Box } from '@mui/material';
 
 
