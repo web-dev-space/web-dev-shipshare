cbe34fe71cd01e82d158728f646a74a5eada8951
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-05 16:06:10 -0700

    feat: init table

--- src/Pages/4-Shipments/EnhancedTable.js
+++ src/Pages/4-Shipments/EnhancedTable.js
@@ -13,39 +13,28 @@ import TableSortLabel from '@mui/material/TableSortLabel';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
 import Paper from '@mui/material/Paper';
-import Checkbox from '@mui/material/Checkbox';
 import IconButton from '@mui/material/IconButton';
 import Tooltip from '@mui/material/Tooltip';
 import FormControlLabel from '@mui/material/FormControlLabel';
 import Switch from '@mui/material/Switch';
-import DeleteIcon from '@mui/icons-material/Delete';
 import FilterListIcon from '@mui/icons-material/FilterList';
 import {visuallyHidden} from '@mui/utils';
+import Button from '@mui/material/Button';
 
-function createData(name, calories, fat, carbs, protein) {
+function createData(trackingNo, route, joinDate, pickupAt, status) {
   return {
-    name,
-    calories,
-    fat,
-    carbs,
-    protein,
+    trackingNo,
+    route,
+    joinDate,
+    pickupAt,
+    status,
   };
 }
 
 const rows = [
-  createData('Cupcake', 305, 3.7, 67, 4.3),
-  createData('Donut', 452, 25.0, 51, 4.9),
-  createData('Eclair', 262, 16.0, 24, 6.0),
-  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
-  createData('Gingerbread', 356, 16.0, 49, 3.9),
-  createData('Honeycomb', 408, 3.2, 87, 6.5),
-  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
-  createData('Jelly Bean', 375, 0.0, 94, 0.0),
-  createData('KitKat', 518, 26.0, 65, 7.0),
-  createData('Lollipop', 392, 0.2, 98, 0.0),
-  createData('Marshmallow', 318, 0, 81, 2.0),
-  createData('Nougat', 360, 19.0, 9, 37.0),
-  createData('Oreo', 437, 18.0, 63, 4.0),
+  createData('YT7136320603122', 'Air Sensitive', 'Mar 12, 2023', 'San Jose', 'Arrived',),
+  createData('YT7136320603122', 'Air Sensitive', 'Mar 12, 2023', 'San Francisco', 'In Shipping',),
+  createData('YT7136320603122', 'Air Sensitive', 'Mar 12, 2023', 'Sunnyvale', 'Packed'),
 ];
 
 function descendingComparator(a, b, orderBy) {
@@ -64,10 +53,6 @@ function getComparator(order, orderBy) {
     : (a, b) => -descendingComparator(a, b, orderBy);
 }
 
-// Since 2020 all major browsers ensure sort stability with Array.prototype.sort().
-// stableSort() brings sort stability to non-modern browsers (notably IE11). If you
-// only support modern browsers you can replace stableSort(exampleArray, exampleComparator)
-// with exampleArray.slice().sort(exampleComparator)
 function stableSort(array, comparator) {
   const stabilizedThis = array.map((el, index) => [el, index]);
   stabilizedThis.sort((a, b) => {
@@ -81,69 +66,34 @@ function stableSort(array, comparator) {
 }
 
 const headCells = [
-  {
-    id: 'name',
-    numeric: false,
-    disablePadding: true,
-    label: 'Dessert (100g serving)',
-  },
-  {
-    id: 'calories',
-    numeric: true,
-    disablePadding: false,
-    label: 'Calories',
-  },
-  {
-    id: 'fat',
-    numeric: true,
-    disablePadding: false,
-    label: 'Fat (g)',
-  },
-  {
-    id: 'carbs',
-    numeric: true,
-    disablePadding: false,
-    label: 'Carbs (g)',
-  },
-  {
-    id: 'protein',
-    numeric: true,
-    disablePadding: false,
-    label: 'Protein (g)',
-  },
+  {id: 'trackingNo', numeric: false, disablePadding: true, label: 'Tracking No.'},
+  {id: 'route', numeric: false, disablePadding: false, label: 'Route'},
+  {id: 'joinDate', numeric: false, disablePadding: false, label: 'Join Date'},
+  {id: 'pickupAt', numeric: false, disablePadding: false, label: 'Pickup At'},
+  {id: 'status', numeric: false, disablePadding: false, label: 'Status'},
+  {id: 'actions', numeric: false, disablePadding: false, label: 'Actions'},
 ];
 
 const DEFAULT_ORDER = 'asc';
-const DEFAULT_ORDER_BY = 'calories';
+const DEFAULT_ORDER_BY = 'trackingNo';
 const DEFAULT_ROWS_PER_PAGE = 5;
 
 function EnhancedTableHead(props) {
-  const {onSelectAllClick, order, orderBy, numSelected, rowCount, onRequestSort} =
-    props;
+  const {order, orderBy, onRequestSort} = props;
   const createSortHandler = (newOrderBy) => (event) => {
     onRequestSort(event, newOrderBy);
   };
 
   return (
     <TableHead>
-      <TableRow>
-        <TableCell padding="checkbox">
-          <Checkbox
-            color="primary"
-            indeterminate={numSelected > 0 && numSelected < rowCount}
-            checked={rowCount > 0 && numSelected === rowCount}
-            onChange={onSelectAllClick}
-            inputProps={{
-              'aria-label': 'select all desserts',
-            }}
-          />
-        </TableCell>
+      <TableRow style={{borderTop: '1px solid #EDF2F7'}}>
         {headCells.map((headCell) => (
           <TableCell
             key={headCell.id}
             align={headCell.numeric ? 'right' : 'left'}
             padding={headCell.disablePadding ? 'none' : 'normal'}
             sortDirection={orderBy === headCell.id ? order : false}
+            style={{backgroundColor: 'white'}}
           >
             <TableSortLabel
               active={orderBy === headCell.id}
@@ -165,73 +115,31 @@ function EnhancedTableHead(props) {
 }
 
 EnhancedTableHead.propTypes = {
-  numSelected: PropTypes.number.isRequired,
   onRequestSort: PropTypes.func.isRequired,
-  onSelectAllClick: PropTypes.func.isRequired,
   order: PropTypes.oneOf(['asc', 'desc']).isRequired,
   orderBy: PropTypes.string.isRequired,
-  rowCount: PropTypes.number.isRequired,
 };
 
-function EnhancedTableToolbar(props) {
-  const {numSelected} = props;
-
+function EnhancedTableToolbar() {
   return (
     <Toolbar
       sx={{
         pl: {sm: 2},
         pr: {xs: 1, sm: 1},
-        ...(numSelected > 0 && {
-          bgcolor: (theme) =>
-            alpha(theme.palette.primary.main, theme.palette.action.activatedOpacity),
-        }),
       }}
     >
-      {numSelected > 0 ? (
-        <Typography
-          sx={{flex: '1 1 100%'}}
-          color="inherit"
-          variant="subtitle1"
-          component="div"
-        >
-          {numSelected} selected
-        </Typography>
-      ) : (
-        <Typography
-          sx={{flex: '1 1 100%'}}
-          variant="h6"
-          id="tableTitle"
-          component="div"
-        >
-          Nutrition
-        </Typography>
-      )}
-
-      {numSelected > 0 ? (
-        <Tooltip title="Delete">
-          <IconButton>
-            <DeleteIcon/>
-          </IconButton>
-        </Tooltip>
-      ) : (
-        <Tooltip title="Filter list">
-          <IconButton>
-            <FilterListIcon/>
-          </IconButton>
-        </Tooltip>
-      )}
+      <Tooltip title="Filter list">
+        <IconButton>
+          <FilterListIcon/>
+        </IconButton>
+      </Tooltip>
     </Toolbar>
   );
 }
 
-EnhancedTableToolbar.propTypes = {
-  numSelected: PropTypes.number.isRequired,
-};
-
 export default function EnhancedTable() {
   const [order, setOrder] = React.useState(DEFAULT_ORDER);
   const [orderBy, setOrderBy] = React.useState(DEFAULT_ORDER_BY);
-  const [selected, setSelected] = React.useState([]);
   const [page, setPage] = React.useState(0);
   const [dense, setDense] = React.useState(false);
   const [visibleRows, setVisibleRows] = React.useState(null);
@@ -270,35 +178,6 @@ export default function EnhancedTable() {
     [order, orderBy, page, rowsPerPage],
   );
 
-  const handleSelectAllClick = (event) => {
-    if (event.target.checked) {
-      const newSelected = rows.map((n) => n.name);
-      setSelected(newSelected);
-      return;
-    }
-    setSelected([]);
-  };
-
-  const handleClick = (event, name) => {
-    const selectedIndex = selected.indexOf(name);
-    let newSelected = [];
-
-    if (selectedIndex === -1) {
-      newSelected = newSelected.concat(selected, name);
-    } else if (selectedIndex === 0) {
-      newSelected = newSelected.concat(selected.slice(1));
-    } else if (selectedIndex === selected.length - 1) {
-      newSelected = newSelected.concat(selected.slice(0, -1));
-    } else if (selectedIndex > 0) {
-      newSelected = newSelected.concat(
-        selected.slice(0, selectedIndex),
-        selected.slice(selectedIndex + 1),
-      );
-    }
-
-    setSelected(newSelected);
-  };
-
   const handleChangePage = React.useCallback(
     (event, newPage) => {
       setPage(newPage);
@@ -311,7 +190,6 @@ export default function EnhancedTable() {
 
       setVisibleRows(updatedRows);
 
-      // Avoid a layout jump when reaching the last page with empty rows.
       const numEmptyRows =
         newPage > 0 ? Math.max(0, (1 + newPage) * rowsPerPage - rows.length) : 0;
 
@@ -336,7 +214,6 @@ export default function EnhancedTable() {
 
       setVisibleRows(updatedRows);
 
-      // There is no layout jump to handle on the first page.
       setPaddingHeight(0);
     },
     [order, orderBy],
@@ -346,12 +223,25 @@ export default function EnhancedTable() {
     setDense(event.target.checked);
   };
 
-  const isSelected = (name) => selected.indexOf(name) !== -1;
+  function getStatusColor(row) {
+    switch (row.status.toLowerCase()) {
+      case 'arrived':
+        return '#EEBD5E';
+      case 'in shipping':
+        return '#FFE03F';
+      case 'packed':
+        return '#80B213';
+      case 'order placed':
+        return '#1A202C';
+      default:
+        return '';
+    }
+  }
 
   return (
     <Box sx={{width: '100%'}}>
       <Paper sx={{width: '100%', mb: 2}}>
-        <EnhancedTableToolbar numSelected={selected.length}/>
+        <EnhancedTableToolbar/>
         <TableContainer>
           <Table
             sx={{minWidth: 750}}
@@ -359,51 +249,37 @@ export default function EnhancedTable() {
             size={dense ? 'small' : 'medium'}
           >
             <EnhancedTableHead
-              numSelected={selected.length}
               order={order}
               orderBy={orderBy}
-              onSelectAllClick={handleSelectAllClick}
               onRequestSort={handleRequestSort}
-              rowCount={rows.length}
             />
             <TableBody>
               {visibleRows
                 ? visibleRows.map((row, index) => {
-                  const isItemSelected = isSelected(row.name);
-                  const labelId = `enhanced-table-checkbox-${index}`;
-
                   return (
-                    <TableRow
-                      hover
-                      onClick={(event) => handleClick(event, row.name)}
-                      role="checkbox"
-                      aria-checked={isItemSelected}
-                      tabIndex={-1}
-                      key={row.name}
-                      selected={isItemSelected}
-                      sx={{cursor: 'pointer'}}
-                    >
-                      <TableCell padding="checkbox">
-                        <Checkbox
-                          color="primary"
-                          checked={isItemSelected}
-                          inputProps={{
-                            'aria-labelledby': labelId,
-                          }}
-                        />
+                    <TableRow hover
+                              tabIndex={-1}
+                              key={row.trackingNo}
+                              style={{
+                                borderTop: '1px solid #EDF2F7',
+                                borderBottom: '1px solid #EDF2F7',
+                              }}>
+                      <TableCell component="th" scope="row" padding="none">
+                        {row.trackingNo}
                       </TableCell>
-                      <TableCell
-                        component="th"
-                        id={labelId}
-                        scope="row"
-                        padding="none"
-                      >
-                        {row.name}
+                      <TableCell align="left">{row.route}</TableCell>
+                      <TableCell align="left">{row.joinDate}</TableCell>
+                      <TableCell align="left">{row.pickupAt}</TableCell>
+                      <TableCell align="left"
+                                 style={{color: getStatusColor(row)}}>
+                        {row.status}
                       </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
+                      <TableCell align="left">
+                        <Button variant="contained"
+                                color='info'
+                        >
+                          Detail</Button>
+                      </TableCell>
                     </TableRow>
                   );
                 })
--- src/Pages/4-Shipments/Shipment-Main.js
+++ src/Pages/4-Shipments/Shipment-Main.js
@@ -33,7 +33,7 @@ const ShipmentMainPage = () => {
         {/*--------------Main Content----------------------*/}
         <Main>
           <Container maxWidth={false}>
-            <Typography variant="h3" component="h1" paragraph>
+            <Typography variant="h4" component="h1" paragraph>
               Shipments
             </Typography>
             <EnhancedTable/>
