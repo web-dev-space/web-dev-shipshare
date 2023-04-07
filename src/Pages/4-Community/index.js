bfea44eaaf420ca6b79517494b124c32aea8f280
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-07 00:43:25 -0700

    feat: implement the `PostDetail` page

--- src/Pages/4-Community/index.js
+++ src/Pages/4-Community/index.js
@@ -2,6 +2,7 @@ import {Route, Router, Routes} from "react-router";
 import Following from "./Follow";
 import Discover from "./Discover";
 import Profile from "./Profile";
+import PostDetails from "./PostDetails";
 
 export function Community() {
     return (
@@ -11,6 +12,7 @@ export function Community() {
                 <Route path="/discover"    element={<Discover/>}/>
                 <Route path="/follow"    element={<Following/>}/>
                 <Route path="/profile" element={<Profile/>}/>
+                <Route path="/posts" element={<PostDetails />}/>
             </Routes>
         </div>
     )
