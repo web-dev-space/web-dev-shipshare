4c5b3ddcae3d7e8a190f66e07d254dbd8abc8246
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-08 00:54:55 -0700

    refactor: clean code

--- src/third-party/layouts/dashboard/header/Header.js
+++ src/third-party/layouts/dashboard/header/Header.js
@@ -59,7 +59,6 @@ export default function Header({ onOpenNav }) {
         justifyContent="flex-end"
         spacing={{ xs: 0.5, sm: 1.5 }}
       >
-        <NotificationsPopover />
         <AccountPopover />
       </Stack>
     </>
