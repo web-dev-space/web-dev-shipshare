51c2973c7c712054b97fc534e4ebe4b744afbccf
CuichanWu <wu.cu@northeastern.edu>
2023-04-08 17:44:43 -0700

    feat: merge address form into one line

--- src/Pages/5-Account/UserEditNewForm.js
+++ src/Pages/5-Account/UserEditNewForm.js
@@ -126,17 +126,17 @@ export default function UserNewEditForm({ isEdit = false, currentUser }) {
 		<FormProvider methods={methods} onSubmit={handleSubmit(onSubmit)}>
 			<Grid container spacing={3}>
 				<Grid item xs={12} md={4}>
-					<Card sx={{ pt: 10, pb: 5, px: 3 }}>
+					<Card sx={{ pt: 6, pb: 5, px: 3 }}>
 						{isEdit && (
 							<Label
 								color={values.status === 'active' ? 'success' : 'error'}
-								sx={{ textTransform: 'uppercase', position: 'absolute', top: 34, right: 24 }}
+								sx={{ textTransform: 'uppercase', position: 'absolute', top: 24, right: 24 }}
 							>
 								{defaultValues.role}
 							</Label>
 						)}
 
-						<Box sx={{ mb: 8 , mt:3}}>
+						<Box sx={{ mb: 4 , mt:3}}>
 							<RHFUploadAvatar
 								name="avatarUrl"
 								maxSize={3145728}
@@ -159,59 +159,13 @@ export default function UserNewEditForm({ isEdit = false, currentUser }) {
 							/>
 						</Box>
 
-						{/*{isEdit && (*/}
-						{/*	<FormControlLabel*/}
-						{/*		labelPlacement="start"*/}
-						{/*		control={*/}
-						{/*			<Controller*/}
-						{/*				name="status"*/}
-						{/*				control={control}*/}
-						{/*				render={({ field }) => (*/}
-						{/*					<Switch*/}
-						{/*						{...field}*/}
-						{/*						checked={field.value !== 'active'}*/}
-						{/*						onChange={(event) =>*/}
-						{/*							field.onChange(event.target.checked ? 'banned' : 'active')*/}
-						{/*						}*/}
-						{/*					/>*/}
-						{/*				)}*/}
-						{/*			/>*/}
-						{/*		}*/}
-						{/*		label={*/}
-						{/*			<>*/}
-						{/*				<Typography variant="subtitle2" sx={{ mb: 0.5 }}>*/}
-						{/*					Banned*/}
-						{/*				</Typography>*/}
-						{/*				<Typography variant="body2" sx={{ color: 'text.secondary' }}>*/}
-						{/*					Apply disable account*/}
-						{/*				</Typography>*/}
-						{/*			</>*/}
-						{/*		}*/}
-						{/*		sx={{ mx: 0, mb: 3, width: 1, justifyContent: 'space-between' }}*/}
-						{/*	/>*/}
-						{/*)}*/}
-
-						{/*<RHFSwitch*/}
-						{/*	name="isVerified"*/}
-						{/*	labelPlacement="start"*/}
-						{/*	label={*/}
-						{/*		<>*/}
-						{/*			<Typography variant="subtitle2" sx={{ mb: 0.5 }}>*/}
-						{/*				Email Verified*/}
-						{/*			</Typography>*/}
-						{/*			<Typography variant="body2" sx={{ color: 'text.secondary' }}>*/}
-						{/*				Disabling this will automatically send the user a verification email*/}
-						{/*			</Typography>*/}
-						{/*		</>*/}
-						{/*	}*/}
-						{/*	sx={{ mx: 0, width: 1, justifyContent: 'space-between' }}*/}
-						{/*/>*/}
 					</Card>
 				</Grid>
 
 				<Grid item xs={12} md={8}>
 					<Card sx={{ p: 3 }}>
 						<Box
+							style={{marginBottom:20}}
 							rowGap={3}
 							columnGap={2}
 							display="grid"
@@ -221,22 +175,11 @@ export default function UserNewEditForm({ isEdit = false, currentUser }) {
 							}}
 						>
 							<RHFTextField name="name" label="Full Name" />
-							<RHFTextField name="email" label="Email Address" />
 							<RHFTextField name="phoneNumber" label="Phone Number" />
 
-							<RHFSelect native name="country" label="Country" placeholder="Country">
-								<option value="" />
-								{countries.map((country) => (
-									<option key={country.code} value={country.label}>
-										{country.label}
-									</option>
-								))}
-							</RHFSelect>
-
-							<RHFTextField name="state" label="State/Region" />
-							<RHFTextField name="city" label="City" />
-							<RHFTextField name="address" label="Address" />
 						</Box>
+						<RHFTextField name="email" label="Email Address" style={{marginBottom:20}}/>
+						<RHFTextField name="address" label="Address" style={{marginBottom:24}}/>
 
 						<Stack alignItems="flex-end" sx={{ mt: 3 }}>
 							<LoadingButton type="submit" variant="contained" loading={isSubmitting}>
