250d8ec8b055870a130211d00f0d758fe21929f6
CuichanWu <wu.cu@northeastern.edu>
2023-04-10 21:46:37 -0700

    feat: add imgs

--- src/Pages/Buyer/6-Help/Tutorials.js
+++ src/Pages/Buyer/6-Help/Tutorials.js
@@ -1,9 +1,50 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Header from "../../../third-party/layouts/dashboard/header"
 import NavVertical from "../../../third-party/layouts/dashboard/nav/NavVertical"
 import Main from "../../../third-party/layouts/dashboard/Main"
 import { Container, Typography, Box } from '@mui/material';
 
+const content = [
+    {
+        title: "Tutorial",
+        subtitle: "This tutorial consists of six steps. \nLet's walk through how to use our service step by step.",
+        imageSource: require('../../../assets/TutorialScreen/tutorial.png')
+    },
+    {
+        title: "Ship to the Warehouse",
+        subtitle: "Shop online and have them shipped to our warehouse address. \nBe sure to include the warehouse address as the shipping address for your package.",
+        imageSource: require('../../../assets/TutorialScreen/shiptothewarehouse.png')
+    },
+    {
+        title: "Add Parcels",
+        subtitle: "Once your package has been shipped, \n" +
+          "you will receive a tracking number.\n" +
+          "Fill in the tracking number in My Parcels page to keep track of your package.",
+        imageSource: require('../../../assets/images/addparcels.png')
+    },
+    {
+        title: "Track Parcels",
+        subtitle: "Stay updated on your parcel’s location and delivery status in My Parcels. \nOnce your package arrives at our warehouse, we'll weigh it and provide you with its weight information.",
+        imageSource: require('../../../assets/images/trackparcels.png')
+    },
+    {
+        title: "Join or Create a Group",
+        subtitle: "Choose a group which destination is close to you and fits your preferred shipping method (such as air or sea, standard or sensitive line). \nIf there is no suitable group available, you can create your own.",
+        imageSource: require('../../../assets/images/joinorcreateagroup.png')
+    },
+    {
+        title: "Add Parcels to the Group",
+        subtitle:
+          "After joining a group, you can select which of your packages that have arrived at our warehouse to add to the group, and pay for the total weight. " +
+          "Once payment is confirmed, we will prepare the package for shipment.",
+        imageSource: require('../../../assets/images/AddParcelsToTheGroup.png')
+    },
+    {
+        title: "Pick Up Your Parcels",
+        subtitle: "You can track the group shipment in real time on the Shipments page. \nOnce the group shipment arrives, you will be notified to pick up your package from the group leader's location.",
+        imageSource: require('../../../assets/images/pickupyourparcels.png')
+    },
+]
 
 const Tutorials = () => {
     const [open, setOpen] = useState(false);
