b65c44a812ce3fe368dac70da4f0fff056936cec
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-11 15:14:56 -0700

    feat: add `shipGroups` apis

---
+++ src/redux/shipGroups/shipGroups-service.js
@@ -0,0 +1,38 @@
+import axios from 'axios';
+
+import {API_BASE} from '../API_BASE';
+const PARCELS_API = `${API_BASE}/shipGroups`;
+
+export const createShipGroup = async (shipGroup) => {
+    const response = await axios.post(PARCELS_API, shipGroup);
+    return response.data;
+}
+
+export const findAllShipGroups = async () => {
+    const response = await axios.get(PARCELS_API);
+    const shipGroups = response.data;
+    return shipGroups;
+}
+
+export const findShipGroupById = async (id) => {
+    const response = await axios.get(`${PARCELS_API}/${id}`);
+    const shipGroup = response.data;
+    return shipGroup;
+}
+
+export const findShipGroupByTrackingNumber = async (trackingNumber) => {
+    const response = await axios.get(`${PARCELS_API}/trackingNumber/${trackingNumber}`);
+    const shipGroup = response.data;
+    return shipGroup;
+}
+
+export const deleteShipGroup = async (id) => {
+    const response = await axios.delete(`${PARCELS_API}/${id}`);
+    return response.data;
+}
+
+export const updateShipGroup = async (shipGroup) => {
+    const id = shipGroup._id;
+    const response = await axios.put(`${PARCELS_API}/${id}`, shipGroup);
+    return response.data;
+}
