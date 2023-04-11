6ff17404269da13f720dbc1198cffb179577f029
wantingxu <xu.wan@northeastern.edu>
2023-04-11 15:14:25 -0700

    feat: `formGroup` function (incomplete)

---
+++ src/Pages/Buyer/3-Groups/RHFDatePicker.js
@@ -0,0 +1,42 @@
+import PropTypes from 'prop-types';
+// form
+import {useFormContext, Controller} from 'react-hook-form';
+// @mui/lab
+import {DatePicker} from '@mui/lab';
+import {TextField} from "@mui/material";
+import {AdapterDayjs} from "@mui/x-date-pickers/AdapterDayjs";
+import {LocalizationProvider} from "@mui/x-date-pickers/LocalizationProvider";
+
+// ----------------------------------------------------------------------
+
+RHFDatePicker.propTypes = {
+  name: PropTypes.string,
+  helperText: PropTypes.node,
+};
+
+export default function RHFDatePicker({name, helperText, ...other}) {
+  const {control} = useFormContext();
+
+  return (
+    <Controller
+      name={name}
+      control={control}
+      render={({field, fieldState: {error}}) => (
+        <LocalizationProvider dateAdapter={AdapterDayjs}>
+          <DatePicker
+            {...field}
+            renderInput={(params) => (
+              <TextField
+                {...params}
+                fullWidth
+                error={!!error}
+                helperText={error ? error?.message : helperText}
+              />
+            )}
+            {...other}
+          />
+        </LocalizationProvider>
+      )}
+    />
+  );
+}
