c3d50a03aca8248132abad21ae477c1ef76f5cb0
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-12 16:37:19 -0700

    chore: update sample shipments

--- src/sampleData/shipments.js
+++ src/sampleData/shipments.js
@@ -1,321 +1,230 @@
-export const shipments = [{
-  "_id": {
-    "$oid": "6411436c04c6777b3e65ca93"
+[
+  {
+    "_id": "643647bc4ff205ed01e7401f",
+    "name": "Dress",
+    "shipRoute": "Air Sensitive",
+    "shipEndDate": "2022-10-11T08:00:00.000Z",
+    "pickupLocation": {
+      "name": "Jane",
+      "address": "North 5th Street, San Jose, CA, 95112",
+      "_id": "643647bc4ff205ed01e74020"
+    },
+    "phoneNumber": "1234567890",
+    "trackingNumber": "YT7148820407528",
+    "leader": "test@test.com",
+    "members": [],
+    "phaseNumber": 0
   },
-  "created": {
-    "$date": {
-      "$numberLong": "1678852972337"
-    }
+  {
+    "_id": "6411436c04c6777b3e65ca93",
+    "created": "2023-03-15T04:02:52.337Z",
+    "modified": "2023-04-02T23:46:45.416Z",
+    "name": "Happy Buy Buy",
+    "shipRoute": "Sea Standard",
+    "shipEndDate": "2023-01-21T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "Jasmine",
+      "address": "101 East Santa Clara Street, San Jose, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.3374561,
+      "geoLongitude": -121.8884615,
+      "shortAddress": "San Jose, CA",
+      "_id": "64374069f80175054b43c102"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [
+      "test@test.com"
+    ],
+    "trackingNumber": "2007364310",
+    "phaseNumber": 3,
+    "totalWeight": 13,
+    "phoneNumber": "6508809999"
   },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680479205416"
-    }
+  {
+    "_id": "641143f6bd7c774187a11f06",
+    "created": "2023-03-15T04:05:10.006Z",
+    "modified": "2023-04-04T19:32:14.047Z",
+    "name": "Crazy Shoppers",
+    "shipRoute": "Air Sensitive",
+    "shipEndDate": "2023-03-30T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "Jasmine",
+      "address": "2051 Stierlin Court, Sunnyvale, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.4239737,
+      "geoLongitude": -122.0705172,
+      "shortAddress": "Sunnyvale, CA",
+      "_id": "64374069f80175054b43c103"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [
+      "test@test.com"
+    ],
+    "phaseNumber": 3,
+    "totalWeight": 2.8,
+    "phoneNumber": "6509907654",
+    "trackingNumber": "3342476771"
   },
-  "name": "Happy Buy Buy",
-  "shipRoute": "Sea Standard",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1674327600000"
-    }
+  {
+    "_id": "64114c2104c6777b3e65ca94",
+    "created": "2023-03-15T04:40:01.814Z",
+    "modified": "2023-04-03T00:20:07.325Z",
+    "name": "Santa Clara Group",
+    "shipRoute": "Sea Standard",
+    "shipEndDate": "2023-05-03T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "Jasmine",
+      "address": "Santa Clara Marriott, Mission College Boulevard, Santa Clara, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.3905822,
+      "geoLongitude": -121.9742264,
+      "shortAddress": "Santa Clara, CA",
+      "_id": "64374069f80175054b43c104"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [
+      "test@test.com"
+    ],
+    "phaseNumber": 1,
+    "totalWeight": 1.7,
+    "phoneNumber": "6509901234"
   },
-  "pickupLocation": {
-    "name": "Jasmine",
-    "address": "101 East Santa Clara Street, San Jose, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.3374561,
-    "geoLongitude": -121.8884615,
-    "shortAddress": "San Jose, CA"
+  {
+    "_id": "64114c6bbd7c774187a11f07",
+    "created": "2023-03-15T04:41:15.316Z",
+    "modified": "2023-03-15T04:41:15.316Z",
+    "name": "Snack Lovers",
+    "shipRoute": "Air Sensitive",
+    "shipEndDate": "2023-05-12T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "Jasmine Lee",
+      "address": "5093 Disk Drive, San Jose, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.4275749,
+      "geoLongitude": -121.9663161,
+      "shortAddress": "San Jose, CA",
+      "_id": "64373a79f80175054b43c035"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [],
+    "phaseNumber": 2,
+    "totalWeight": 0
   },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [
-    "test@test.com"
-  ],
-  "trackingNumber": "2007364310",
-  "phaseNumber": 3,
-  "totalWeight": 13,
-  "phoneNumber": "6508809999"
-},{
-  "_id": {
-    "$oid": "641143f6bd7c774187a11f06"
+  {
+    "_id": "64129c5a2d97dfd382bc4d57",
+    "created": "2023-03-16T04:34:34.650Z",
+    "modified": "2023-03-16T04:34:34.650Z",
+    "name": "Cupertino Shoppers",
+    "shipRoute": "Sea Standard",
+    "shipEndDate": "2023-05-24T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "Thomas Wang",
+      "address": "85°C Bakery Cafe - Cupertino, Stevens Creek Boulevard, Cupertino, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.32390399999999,
+      "geoLongitude": -122.011104,
+      "shortAddress": "Cupertino, CA",
+      "_id": "64373a79f80175054b43c036"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [
+      "test@test.com"
+    ],
+    "totalWeight": 3.9,
+    "phoneNumber": "5509908987",
+    "phaseNumber": 4
   },
-  "created": {
-    "$date": {
-      "$numberLong": "1678853110006"
-    }
+  {
+    "_id": "6424eb33d0563c9059222bcc",
+    "name": "Toy Lovers",
+    "shipRoute": "Air Standard",
+    "shipEndDate": "2023-04-26T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "test@test.com",
+      "address": "389 East Arques Avenue, Sunnyvale, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.3828992,
+      "geoLongitude": -122.0230271,
+      "shortAddress": "Sunnyvale, CA",
+      "_id": "64374069f80175054b43c105"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [],
+    "created": "2023-03-30T01:51:47.014Z",
+    "modified": "2023-03-30T01:51:47.014Z",
+    "totalWeight": 0,
+    "phaseNumber": 0
   },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680636734047"
-    }
+  {
+    "_id": "64265c52d15c593394908b55",
+    "name": "Wonderful Ship",
+    "shipRoute": "Air Standard",
+    "shipEndDate": "2023-04-29T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "test@test.com",
+      "address": "453 Ravendale Drive, Sunnyvale, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.390518,
+      "geoLongitude": -122.0511617,
+      "shortAddress": "Sunnyvale, CA",
+      "_id": "64374069f80175054b43c106"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [],
+    "created": "2023-03-31T04:06:42.475Z",
+    "modified": "2023-03-31T04:06:42.475Z",
+    "totalWeight": 0,
+    "phaseNumber": 0
   },
-  "name": "Crazy Shoppers",
-  "shipRoute": "Air Sensitive",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1680202800000"
-    }
+  {
+    "_id": "6429e8e725a20bc0d2aab387",
+    "name": "Wonderland",
+    "shipRoute": "Sea Standard",
+    "shipEndDate": "2023-05-05T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "www@mail.com",
+      "address": "376 Garcia Avenue, Mountain View, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.4238729,
+      "geoLongitude": -122.090429,
+      "shortAddress": "Mountain View, CA",
+      "_id": "64374069f80175054b43c107"
+    },
+    "user": "www@mail.com",
+    "courier": "dhl",
+    "members": [],
+    "created": "2023-04-02T20:43:19.069Z",
+    "modified": "2023-04-02T20:43:19.069Z",
+    "totalWeight": 0,
+    "phaseNumber": 0
   },
-  "pickupLocation": {
-    "name": "Jasmine",
-    "address": "2051 Stierlin Court, Sunnyvale, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.4239737,
-    "geoLongitude": -122.0705172,
-    "shortAddress": "Sunnyvale, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [
-    "test@test.com"
-  ],
-  "phaseNumber": 3,
-  "totalWeight": 2.8,
-  "phoneNumber": "6509907654",
-  "trackingNumber": "3342476771"
-},{
-  "_id": {
-    "$oid": "64114c2104c6777b3e65ca94"
-  },
-  "created": {
-    "$date": {
-      "$numberLong": "1678855201814"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680481207325"
-    }
-  },
-  "name": "Santa Clara Group",
-  "shipRoute": "Sea Standard",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1683140400000"
-    }
-  },
-  "pickupLocation": {
-    "name": "Jasmine",
-    "address": "Santa Clara Marriott, Mission College Boulevard, Santa Clara, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.3905822,
-    "geoLongitude": -121.9742264,
-    "shortAddress": "Santa Clara, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [
-    "test@test.com"
-  ],
-  "phaseNumber": 1,
-  "totalWeight": 1.7,
-  "phoneNumber": "6509901234"
-},{
-  "_id": {
-    "$oid": "64114c6bbd7c774187a11f07"
-  },
-  "created": {
-    "$date": {
-      "$numberLong": "1678855275316"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1678855275316"
-    }
-  },
-  "name": "Snack Lovers",
-  "shipRoute": "Air Sensitive",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1683918000000"
-    }
-  },
-  "pickupLocation": {
-    "name": "Jasmine Lee",
-    "address": "5093 Disk Drive, San Jose, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.4275749,
-    "geoLongitude": -121.9663161,
-    "shortAddress": "San Jose, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [],
-  "phaseNumber": 0,
-  "totalWeight": 0
-},{
-  "_id": {
-    "$oid": "64129c5a2d97dfd382bc4d57"
-  },
-  "created": {
-    "$date": {
-      "$numberLong": "1678941274650"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1678941274650"
-    }
-  },
-  "name": "Cupertino Shoppers",
-  "shipRoute": "Sea Standard",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1684954800000"
-    }
-  },
-  "pickupLocation": {
-    "name": "Thomas Wang",
-    "address": "85°C Bakery Cafe - Cupertino, Stevens Creek Boulevard, Cupertino, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.32390399999999,
-    "geoLongitude": -122.011104,
-    "shortAddress": "Cupertino, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [
-    "test@test.com"
-  ],
-  "totalWeight": 3.9,
-  "phoneNumber": "5509908987",
-  "phaseNumber": 0
-},{
-  "_id": {
-    "$oid": "6424eb33d0563c9059222bcc"
-  },
-  "name": "Toy Lovers",
-  "shipRoute": "Air Standard",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1682535600000"
-    }
-  },
-  "pickupLocation": {
-    "name": "test@test.com",
-    "address": "389 East Arques Avenue, Sunnyvale, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.3828992,
-    "geoLongitude": -122.0230271,
-    "shortAddress": "Sunnyvale, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [],
-  "created": {
-    "$date": {
-      "$numberLong": "1680141107014"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680141107014"
-    }
-  },
-  "totalWeight": 0,
-  "phaseNumber": 0
-},{
-  "_id": {
-    "$oid": "64265c52d15c593394908b55"
-  },
-  "name": "Wonderful Ship",
-  "shipRoute": "Air Standard",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1682794800000"
-    }
-  },
-  "pickupLocation": {
-    "name": "test@test.com",
-    "address": "453 Ravendale Drive, Sunnyvale, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.390518,
-    "geoLongitude": -122.0511617,
-    "shortAddress": "Sunnyvale, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [],
-  "created": {
-    "$date": {
-      "$numberLong": "1680235602475"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680235602475"
-    }
-  },
-  "totalWeight": 0,
-  "phaseNumber": 0
-},{
-  "_id": {
-    "$oid": "6429e8e725a20bc0d2aab387"
-  },
-  "name": "Wonderland",
-  "shipRoute": "Sea Standard",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1683313200000"
-    }
-  },
-  "pickupLocation": {
-    "name": "www@mail.com",
-    "address": "376 Garcia Avenue, Mountain View, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.4238729,
-    "geoLongitude": -122.090429,
-    "shortAddress": "Mountain View, CA"
-  },
-  "user": "www@mail.com",
-  "courier": "dhl",
-  "members": [],
-  "created": {
-    "$date": {
-      "$numberLong": "1680468199069"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680468199069"
-    }
-  },
-  "totalWeight": 0,
-  "phaseNumber": 0
-},{
-  "_id": {
-    "$oid": "642a6b63d9cee35dbdc12d90"
-  },
-  "name": "Sunnyvale Sea Group",
-  "shipRoute": "Sea Sensitive",
-  "shipEndDate": {
-    "$date": {
-      "$numberLong": "1682881200000"
-    }
-  },
-  "pickupLocation": {
-    "name": "test@test.com",
-    "address": "909 Kifer Road, Sunnyvale, CA, USA",
-    "notes": "",
-    "geoLatitude": 37.3752998,
-    "geoLongitude": -122.0092395,
-    "shortAddress": "Sunnyvale, CA"
-  },
-  "user": "test@test.com",
-  "courier": "dhl",
-  "members": [],
-  "created": {
-    "$date": {
-      "$numberLong": "1680501603757"
-    }
-  },
-  "modified": {
-    "$date": {
-      "$numberLong": "1680501603757"
-    }
-  },
-  "totalWeight": 0,
-  "phaseNumber": 0
-}]
+  {
+    "_id": "642a6b63d9cee35dbdc12d90",
+    "name": "Sunnyvale Sea Group",
+    "shipRoute": "Sea Sensitive",
+    "shipEndDate": "2023-04-30T19:00:00.000Z",
+    "pickupLocation": {
+      "name": "test@test.com",
+      "address": "909 Kifer Road, Sunnyvale, CA, USA",
+      "notes": "",
+      "geoLatitude": 37.3752998,
+      "geoLongitude": -122.0092395,
+      "shortAddress": "Sunnyvale, CA",
+      "_id": "64374069f80175054b43c108"
+    },
+    "user": "test@test.com",
+    "courier": "dhl",
+    "members": [],
+    "created": "2023-04-03T06:00:03.757Z",
+    "modified": "2023-04-03T06:00:03.757Z",
+    "totalWeight": 0,
+    "phaseNumber": 0
+  }
+]
