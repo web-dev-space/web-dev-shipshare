1fb21cefd7bf5dcaeb475173781b6a4d179ee5db
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-12 20:55:01 -0700

    fix: user role error in `ParcelMain` page

--- src/Pages/Buyer/1-Parcels/Parcel-Main.js
+++ src/Pages/Buyer/1-Parcels/Parcel-Main.js
@@ -41,8 +41,7 @@ const ParcelMainPage = () => {
     };
 
     // ---------user role---------
-    const role = useSelector(state => state.auth.role);
-
+    const role = useSelector(state => state.auth.currentUser.role);
 
     // ---------search bar---------
     const [searchTerm, setSearchTerm] = useState('');
