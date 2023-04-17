c901eb46c5ac6275cf5433ae1a8d57e1b2aba27b
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-17 01:08:08 -0700

    feat: post new tracking in tracktry when creating new parcel

--- src/Pages/Buyer/1-Parcels/Parcel-Main.js
+++ src/Pages/Buyer/1-Parcels/Parcel-Main.js
@@ -30,6 +30,7 @@ import {uploadImage} from "api/imageUpload";
 import {Helmet} from "react-helmet";
 import {parcelData} from "../../../sampleData/parcels";
 import {Link} from "react-router-dom";
+import {postNewTracking} from "../../../redux/parcels/parcels-service";
 
 
 
