7009a77449e7e9c0428d7c4f12e785c952543539
CuichanWu <wu.cu@northeastern.edu>
2023-04-16 21:26:55 -0700

    feat: restyle `SignUp` small page layout

--- src/Pages/0-SignIn & SignUp/SignInPage.js
+++ src/Pages/0-SignIn & SignUp/SignInPage.js
@@ -214,6 +214,7 @@ const LoginPage = () => {
               justifyContent: 'center',
               alignItems: 'center',
             }}
+            maxWidth={900}
           >
             <Image
               src={welcomeImg}
