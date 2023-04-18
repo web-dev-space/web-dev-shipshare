eff9fd6ed0432549ffa6c7a9380654cdae8117dd
Yuyan-Lei <lei.yuy@northeastern.edu>
2023-04-18 00:30:37 -0700

    feat: add `SearchParcel` page

--- src/Pages/Buyer/4-Community/index.js
+++ src/Pages/Buyer/4-Community/index.js
@@ -1,9 +1,9 @@
-import {Route, Router, Routes} from "react-router";
-import Following from "Pages/Merchant/0-Dashboard/Dashboard";
+import {Route, Routes} from "react-router";
 import {DiscoverRoute} from "./Discover";
 import Profile from "./Profile";
 import {useSelector} from "react-redux";
 import Page403 from "../../../third-party/ErrorPages/Page403";
+import SearchProducts from "./Products/SearchProducts";
 
 export function Community() {
     const currentUser = useSelector(state => state.auth.currentUser);
@@ -13,6 +13,7 @@ export function Community() {
                 <Route path="/discover/*"    element={<DiscoverRoute/>}/>
                 <Route path="/profile" element={currentUser && currentUser.role === 'buyer' ? <Profile/> : <Page403 />}/>
                 <Route path="/profile/:userId" element={<Profile/>}/>
+                <Route path="/products" element={<SearchProducts/>}/>
             </Routes>
         </div>
     )
