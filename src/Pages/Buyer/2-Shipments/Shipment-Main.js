03a6047463718a2dac22c9ae92b1f6908f381dd9
Zhongyi-Lu <zhongyi_lu@outlook.com>
2023-04-17 17:16:14 -0700

    styles

--- src/Pages/Buyer/2-Shipments/Shipment-Main.js
+++ src/Pages/Buyer/2-Shipments/Shipment-Main.js
@@ -6,14 +6,16 @@ import Header from "../../../third-party/layouts/dashboard/header";
 import NavVertical from "../../../third-party/layouts/dashboard/nav/NavVertical.js";
 import EnhancedTable from "./EnhancedTable.js";
 import { updateShipGroupThunk, findAllShipGroupsThunk } from "redux/shipGroups/shipGroups-thunks.js";
-import {Helmet} from "react-helmet";
+import { Helmet } from "react-helmet";
 import shipGroupsData from "../../../sampleData/shipGroups";
 import "./shipment-main.css";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import { random } from "lodash";
 
 const ShipmentMainPage = () => {
   const dispatch = useDispatch();
+
+  const user = useSelector((state) => state.auth.currentUser);
 
   const [open, setOpen] = useState(false);
 
@@ -33,13 +35,13 @@ const ShipmentMainPage = () => {
   };
 
   // ---------current user---------
-  const currentUser = useSelector(state => state.auth.currentUser || {role: "visitor"});
+  const currentUser = useSelector(state => state.auth.currentUser || { role: "visitor" });
 
   useEffect(() => {
     const initShipments = () => {
-        if (currentUser && currentUser !== "visitor") {
-            dispatch(findAllShipGroupsThunk());
-        }
+      if (currentUser && currentUser !== "visitor") {
+        dispatch(findAllShipGroupsThunk());
+      }
     }
 
     initShipments();
@@ -70,19 +72,19 @@ const ShipmentMainPage = () => {
         <NavVertical openNav={open} onCloseNav={handleClose} />
 
         {/*--------------Main Content----------------------*/}
-          {
-              currentUser.role === "visitor" &&
-              <Typography variant="h5" style={{
-                  position: "absolute",
-                  top: "40%",
-                  left: "60%",
-                  transform: "translate(-50%, -50%)",
-                  textAlign: "center",
-                  zIndex: "1000"
-              }}>
-                  Unfortunately, the parcel feature is currently unavailable while you are in visitor mode. Please <Link to="/login" style={{ color: '#80B213' }}>log in</Link> or <Link to="/signup" style={{ color: '#80B213' }}>sign up</Link> to unlock all the features of our website.
-              </Typography>
-          }
+        {
+          currentUser.role === "visitor" &&
+          <Typography variant="h5" style={{
+            position: "absolute",
+            top: "40%",
+            left: "60%",
+            transform: "translate(-50%, -50%)",
+            textAlign: "center",
+            zIndex: "1000"
+          }}>
+            Unfortunately, the parcel feature is currently unavailable while you are in visitor mode. Please <Link to="/login" style={{ color: '#80B213' }}>log in</Link> or <Link to="/signup" style={{ color: '#80B213' }}>sign up</Link> to unlock all the features of our website.
+          </Typography>
+        }
         <Main className={currentUser.role === "visitor" ? "visitor-mode" : ""}>
           <Container maxWidth={false}>
             <Typography variant="h4" component="h1" paragraph>
